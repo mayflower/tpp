15e8976ba06b1a9769da5e5502ab10411c4312cb
Christian Albrecht <christian.albrecht@mayflower.de>
2013-06-07 16:06:55 +0200

    Unit test listCtrl sort

--- test/unit/listTest.js
+++ test/unit/listTest.js
@@ -8,8 +8,21 @@ describe('the listCtrl', function() {
     ctrl = $controller('listCtrl', {$scope: scope});
   }));
 
-  it('should expect that true is true', function() {
+  it('should have list and criteria properties', function() {
     expect(scope.list).toEqual(jasmine.any(Array));
     expect(scope.criteria).toBe('ipid');
+  });
+
+  it('should set criteria', function() {
+    scope.sort('foo');
+    expect(scope.criteria).toBe('foo');
+  });
+
+  it('should prefix criteria when called twice with same param', function() {
+    scope.sort('foo');
+    scope.sort('foo');
+    expect(scope.criteria).toBe('-foo');
+    scope.sort('bar');
+    expect(scope.criteria).toBe('bar');
   });
 });
