6a1d915f954bbb44b6836ff90cae365e50123aea
Christian Albrecht <christian.albrecht@mayflower.de>
2013-06-07 10:35:53 +0200

    Initial project setup

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: '.'
+                }
+            }
+        },
+        karma: {
+            options: {
+                basePath: '.',
+                proxies: {
+                    '/src': 'http://localhost:8000/src',
+                    '/components': 'http://localhost:8000/components'
+                },
+                singleRun: true,
+                autoWatch: false,
+                logLevel: 'info'
+            },
+            "e2e-dev": {
+                options: {
+                    files: [
+                        'node_modules/karma/adapter/lib/angular-scenario.js',
+                        'node_modules/karma/adapter/angular-scenario.js',
+                        //watch for reload but do not include into html
+                        {pattern: 'components/**/*', included: false},
+                        {pattern: 'src/**/*.html', included: false},
+                        'src/**/*.js',
+                        'test/e2e/**/*.js'
+                    ],
+                    browsers: ['PhantomJS'],
+                    singleRun: false,
+                    autoWatch: true
+                }
+            },
+            "unit-dev": {
+                options: {
+                    files: [
+                        'node_modules/karma/adapter/lib/jasmine.js',
+                        'node_modules/karma/adapter/jasmine.js',
+                        //watch for reload but do not include into html
+                        {pattern: 'components/**/*', included: false},
+                        {pattern: 'src/**/*.html', included: false},
+                        'src/**/*.js',
+                        'test/unit/**/*.js'
+                    ],
+                    browsers: ['PhantomJS'],
+                    singleRun: false,
+                    autoWatch: true
+                }
+            }
+        },
+        mkdir: {
+            all: { options: { create: ['src', 'test/e2e', 'test/unit'] } }
+        }
+    });
+
+
+    // Default task(s).
+    grunt.registerTask('test:unit-dev', ['connect:server', 'karma:unit-dev']);
+    grunt.registerTask('test:e2e-dev',  ['connect:server', 'karma:e2e-dev']);
+
+    grunt.registerTask('setup', ['mkdir:all']);
+
+
+    // Load the plugins provided by npm
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+};
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-schneller-mit-angular
-=====================
+    $ npm install
+    $ node_modules/.bin/bower install
+    $ PHANTOMJS_BIN=node_modules/.bin/phantomjs node_modules/.bin/grunt test:unit-dev
 
-Einfach schneller mit angular
+Put a test in test/unit directory
