b79b70bf9aa5cbcbede8e6996f3a40e593951929
Florian Fackler <florian.fackler@mintao.com>
2013-06-07 14:13:48 +0200

    First semi real list

--- src/controllers/listCtrl.js
+++ src/controllers/listCtrl.js
@@ -1,4 +1,12 @@
 angular.module('schnellerApp')
   .controller('listCtrl', ['$scope', 'dataProvider', function($scope, dataProvider) {
     $scope.list = dataProvider
+    $scope.criteria = 'ipid'
+    $scope.sort = function(criteria) {
+        if ($scope.criteria === criteria) {
+            $scope.criteria = '-' + criteria;
+        } else {
+            $scope.criteria = criteria;
+        }
+    }
   }]);
--- src/services/dataProviderService.js
+++ src/services/dataProviderService.js
@@ -1,13 +1,41 @@
 angular.module('schnellerApp')
   .factory('dataProvider', function() {
-    return [{
-      id: 1,
-      label: 'eins'
-    },{
-      label: 'zwei',
-      id: 2
-    },{
-      label: 'drei',
-      id: 3
-    }]
+        return [
+            {
+                ipid: '123-456-789',
+                approvable: false,
+                approvedBy: "Hans Wurst",
+                exceededSince: 120,
+                provider: "Ajax inc.",
+                status: 'On Hold',
+                price: 123.12
+            },
+            {
+                ipid: '123-456-792',
+                approvable: true,
+                approvedBy: null,
+                exceededSince: 128,
+                provider: "Banana inc.",
+                status: "Accepted",
+                price: 23.10
+            },
+            {
+                ipid: '223-456-780',
+                approvable: false,
+                approvedBy: "Andi Gewehre",
+                exceededSince: 120,
+                provider: "CMax ltd.",
+                status: 'On Hold',
+                price: 1823.12
+            },
+            {
+                ipid: '23-456-789',
+                approvable: true,
+                approvedBy: null,
+                exceededSince: -20,
+                provider: "Ajax inc.",
+                status: 'On Hold',
+                price: 123.12
+            }
+        ]
   });
