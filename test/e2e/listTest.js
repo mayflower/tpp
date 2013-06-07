6101724d59aa3dd21e9a3917c099fc82158551f2
Christian Albrecht <christian.albrecht@mayflower.de>
2013-06-07 13:25:45 +0200

    Repair path to views in $routeProvider

--- test/e2e/listTest.js
+++ test/e2e/listTest.js
@@ -3,6 +3,8 @@ describe('the fuck out of it', function() {
     browser().navigateTo('src/index.html');
   });
 
-  it('must work', function() {
-  })
+  it('should have 3 rows', function() {
+    //browser().navigateTo('#/');
+    expect(repeater('div.list', 'the list').count()).toBe(3);
+  });
 });
