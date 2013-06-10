cc47be77d90b786314c74e67e118e5392f5eefdc
Christian Albrecht <christian.albrecht@mayflower.de>
2013-06-10 08:21:45 +0200

    example directive spec for @scopes

--- test/unit/directives/exampleSpec.js
+++ test/unit/directives/exampleSpec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-describe('example directive', function() {
+ddescribe('example directive', function() {
   var rootScope, compile, interpolate, directive;
 
   angular.module('exampleModule', []);
@@ -45,5 +45,45 @@ describe('example directive', function() {
     });
     rootScope.$apply('foo.bar = 1');
     compile('<one example>')(rootScope);
+  });
+
+  it('@scopes', function() {
+    var watchCalls = 0;
+
+    directive('example', function() {
+      return {
+        scope: {
+          //atScope: '@at-isolated', Error: Invalid isolate scope definition for directive example: @at-isolated
+          atIsolated: '@isolated',
+          atInterpolated: '@interpolated'
+        },
+        link: function(scope, element, attributes) {
+          expect(scope.atIsolated).toBeUndefined();
+          expect(scope.atInterpolated).toBeUndefined();
+
+          scope.$watch('atInterpolated', function(newVal, oldVal) {
+            watchCalls++;
+            if (1 == watchCalls) {
+              expect(newVal).toBe(oldVal);
+              expect(newVal).toBe('1');
+            }
+            if (2 == watchCalls) {
+              expect(oldVal).toBe('1');
+              expect(newVal).toBe('2');
+            }
+          });
+
+          scope.$apply();
+          expect(watchCalls).toBe(1);
+
+          expect(scope.atIsolated).toBe('iso.foo');
+          expect(scope.atInterpolated).toBe('1');
+        }
+      }
+    });
+    rootScope.$apply('iso.foo = 1');
+    compile('<one isolated="iso.foo" interpolated="{{iso.foo}}" example>')(rootScope);
+    rootScope.$apply('iso.foo = 2');
+    expect(watchCalls).toBe(2);
   });
 });
