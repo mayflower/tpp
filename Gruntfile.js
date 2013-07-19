b55c6f47ccc036be44f1895a0cbf819e7e3bba55
Christian Albrecht <christian.albrecht@mayflower.de>
2013-07-19 06:13:33 +0200

    Fix error because of missing configFile, remove grunt-connect
    the config of configFile ois overwritten anyway
    karma has its own server so contrib is not needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,37 @@
+process.env.PHANTOMJS_BIN = __dirname + '/node_modules/phantomjs/lib/phantom/bin/phantomjs';
+
 module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        connect: {
-            server: {
-                options: {
-                    port: 8000,
-                    base: '.'
-                }
-            }
-        },
         karma: {
             options: {
-                basePath: '.',
-                proxies: {
-                    '/': 'http://localhost:8000/',
-                    'src': 'http://localhost:8000/src',
-                    'components': 'http://localhost:8000/components'
-                },
+                // does not serve any purpose but to fix a grunt-karma error
+                configFile: 'node_modules/grunt-karma/karma.conf.js',
+                // setting urlRoot to __dirname serves e2e browser().navigateTo calls
+                urlRoot: __dirname + '/',
+                basePath: __dirname,
                 singleRun: true,
                 autoWatch: false,
-                logLevel: 'info'
+                logLevel: 'info',
+                exclude: [
+                  '**/*.min.js'
+                ]
             },
             "e2e-dev": {
                 options: {
                     files: [
                         'node_modules/karma/adapter/lib/angular-scenario.js',
                         'node_modules/karma/adapter/angular-scenario.js',
-                        //watch for reload but do not include into html
+                        //watch for reload and serve but do NOT include into testing frame with a script tag
                         {pattern: 'components/**/*.js', included: false},
-                        {pattern: 'src/**/*.html', included: false},
+                        {pattern: '**/*.html', included: false},
+                        {pattern: '**/*.css', included: false},
                         'src/**/*.js',
                         'test/e2e/**/*.js'
                     ],
-                    browsers: ['PhantomJS', 'Chrome'],
+                    browsers: ['PhantomJS'],
                     singleRun: false,
                     autoWatch: true
                 }
@@ -44,15 +41,12 @@ module.exports = function(grunt) {
                     files: [
                       'node_modules/karma/adapter/lib/jasmine.js',
                       'node_modules/karma/adapter/jasmine.js',
-                      //watch for reload but do not include into html
-                      {pattern: 'src/**/*.html', included: false},
+                      //watch for reload and serve but do NOT include into testing frame with a script tag
+                      {pattern: '**/*.html', included: false},
                       'components/angular/angular.js',
-                      'components/angular-mocks/angular-mocks.js',
+                      'components/**/*.js',
                       'src/**/*.js',
                       'test/unit/**/*.js'
-                    ],
-                    exclude: [
-                      'components/**/*.min.js',
                     ],
                     browsers: ['PhantomJS'],
                     singleRun: false,
@@ -67,8 +61,8 @@ module.exports = function(grunt) {
 
 
     // Default task(s).
-    grunt.registerTask('test:unit-dev', ['connect:server', 'karma:unit-dev']);
-    grunt.registerTask('test:e2e-dev',  ['connect:server', 'karma:e2e-dev']);
+    grunt.registerTask('test:unit-dev', ['karma:unit-dev']);
+    grunt.registerTask('test:e2e-dev',  ['karma:e2e-dev']);
 
     grunt.registerTask('setup', ['mkdir:all']);
 
