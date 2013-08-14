d8e1b5ee0267289bc6743d81f587fc06fdd52339
Robin Gloster <robin@loc-com.de>
2013-08-14 10:11:49 +0200

    person crud, started ttp table

---
+++ src/Mayflower/WochenplanerBundle/Tests/Controller/WeekControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mayflower\WochenplanerBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class WeekControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/index');
+    }
+
+}
