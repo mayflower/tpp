63a1ca52627784e7e8adaafba585831de49c3019
Robin Gloster <robin@loc-com.de>
2013-08-29 15:36:22 +0200

    [TPP-12] deployment fixes

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,10 +1,10 @@
 set :application, "tpp"
 set :domain,      "lcweb.lc.local"
 set :deploy_to,   "/www/tpp"
-set :deploy_via, :remote_cache
+set :deploy_via,  :remote_cache
 set :app_path,    "app"
 
-set :repository,  "lcgitlab:loccom/#{application}.git"
+set :repository,  "git@lcgitlab:loccom/#{application}.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
@@ -13,11 +13,11 @@ set :model_manager, "doctrine"
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :db, domain, :primary => true # This may be the same as your `Web` server
 
 set   :use_sudo,      false
-set :user, "deploy"
+set  :user, "deploy"
 set  :keep_releases,  3
-default_run_options[:pty] = true
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
