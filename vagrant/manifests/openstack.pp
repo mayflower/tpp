4bedbbe1f2b3eab18f5d53cf4520ea4b430e8db4
Robin Gloster <robin@loc-com.de>
2013-10-01 17:50:04 +0200

    update openstack puppet (flaky)

--- vagrant/manifests/openstack.pp
+++ vagrant/manifests/openstack.pp
@@ -14,66 +14,66 @@ and title != 'software-properties-common'
 Package["libaugeas-ruby"] -> Augeas <| |>
 
 package {
-  [
-    'build-essential',
-    'vim',
-    'curl',
-    'git-core',
-    'libaugeas-ruby',
-  ]:
-  ensure  => 'installed',
-  before  => Class['php::augeas']
+[
+'build-essential',
+'vim',
+'curl',
+'git-core',
+'libaugeas-ruby',
+]:
+ensure  => 'installed',
+before  => Class['php::augeas']
 }
 
 class { 'nginx': }
 
 nginx::resource::vhost { 'tpp.novalocal':
-  ensure       => present,
-  server_name  => ['tpp.novalocal'],
-  listen_port  => 80,
-  index_files  => ['index.html',],
-  www_root     => '/www/tpp/app',
-  try_files   => ['$uri', '$uri/', '/index.html', '=404'],
+ensure       => present,
+server_name  => ['172.18.54.8'],
+listen_port  => 80,
+index_files  => ['index.html',],
+www_root     => '/www/tpp/dist',
+try_files   => ['$uri', '$uri/', '/index.html', '=404'],
 }
 
 $path_translated = 'PATH_TRANSLATED $document_root$fastcgi_path_info'
 $script_filename = 'SCRIPT_FILENAME $document_root$fastcgi_script_name'
 
 nginx::resource::location { 'api-rewrite':
-  ensure              => 'present',
-  vhost               => '172.18.54.8',
-  location            => '~ ^/api.+$',
-  www_root            => '/www/tpp/api/web',
-  proxy               => undef,
-  index_files  => [
-    'app.php',
-  ],
-  location_cfg_append => {'rewrite' => '^(.+)$ /app.php/$1 last',},
-  notify              => Class['nginx::service'],
+ensure              => 'present',
+vhost               => 'tpp.novalocal',
+location            => '~ ^/api.+$',
+www_root            => '/www/tpp/api/web',
+proxy               => undef,
+index_files  => [
+'app.php',
+],
+location_cfg_append => {'rewrite' => '^(.+)$ /app.php/$1 last',},
+notify              => Class['nginx::service'],
 }
 
 nginx::resource::location { 'tpp.dev-php':
-  ensure              => 'present',
-  vhost               => '172.18.54.8',
-  location            => '~ ^/app\.php($|/)',
-  proxy               => undef,
-  www_root            => '/www/tpp/api/web',
-  index_files  => [
-  'app_dev.php',
-  ],
-  location_cfg_append => {
-  'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
-  'fastcgi_param'           => 'PATH_INFO $fastcgi_path_info',
-  'fastcgi_param '          => $path_translated,
-  'fastcgi_param  '         => $script_filename,
-  'fastcgi_pass'            => 'unix:/var/run/php5-fpm.sock',
-  'fastcgi_index'           => 'app.php',
-  'fastcgi_busy_buffers_size' => '512k',
-  'fastcgi_buffer_size'       => '512k',
-  'fastcgi_buffers'           => '16 512k',
-  'include'                 => 'fastcgi_params'
-  },
-  notify              => Class['nginx::service'],
+ensure              => 'present',
+vhost               => 'tpp.novalocal',
+location            => '~ ^/app\.php($|/)',
+proxy               => undef,
+www_root            => '/www/tpp/api/web',
+index_files  => [
+'app_dev.php',
+],
+location_cfg_append => {
+'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
+'fastcgi_param'           => 'PATH_INFO $fastcgi_path_info',
+'fastcgi_param '          => $path_translated,
+'fastcgi_param  '         => $script_filename,
+'fastcgi_pass'            => 'unix:/var/run/php5-fpm.sock',
+'fastcgi_index'           => 'app.php',
+'fastcgi_busy_buffers_size' => '512k',
+'fastcgi_buffer_size'       => '512k',
+'fastcgi_buffers'           => '16 512k',
+'include'                 => 'fastcgi_params'
+},
+notify              => Class['nginx::service'],
 }
 
 include php
@@ -81,27 +81,35 @@ include php::apt
 
 class {
   'php::cli':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::dev':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::pear':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::extension::mysql':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::extension::curl':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::extension::mcrypt':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::extension::intl':
-    ensure => present;
+  ensure => present,
+  provider => 'apt';
   'php::composer': ;
   'php::fpm':
-    ensure   => installed,
-    settings => {
-      set => {
-      'Date/date.timezone' => 'Europe/Berlin',
-      }
-    };
+  ensure   => installed,
+  provider => 'apt',
+  settings => {
+  set => {
+  'Date/date.timezone' => 'Europe/Berlin',
+  }
+  };
 }
 
 php::fpm::conf {'www':
@@ -123,18 +131,20 @@ mysql::db { 'mayflower_tpp':
   require  => Class['mysql::server'],
 }
 
-
-include nodejs
+class { "nodejs":
+  version => '0.10.20-1chl1~quantal1',
+  manage_repo => true,
+}
 
 package { 'bower':
   ensure => present,
   provider => 'npm',
-require => Class['nodejs']
+  require => Class['nodejs']
 }
 package { 'grunt-cli':
   ensure => present,
   provider => 'npm',
-require => Class['nodejs']
+  require => Class['nodejs']
 }
 exec { 'install_node_modules':
   command => 'npm install',
@@ -166,15 +176,6 @@ exec { 'db_schema_create':
   Mysql::Db['mayflower_tpp']
   ]
 }
-exec { 'db_schema_create_test':
-  command => '/www/tpp/api/app/console doctrine:schema:update -e test',
-  cwd     => '/www/tpp/api',
-  user    => 'www-data',
-  require => [
-  Exec['composer_install'],
-  Mysql::Db['mayflower_tpp_tests']
-  ]
-}
 exec { 'assets_install':
   command => '/www/tpp/api/app/console assets:install --relative --symlink',
   cwd     => '/www/tpp/api',
@@ -186,3 +187,4 @@ exec { 'assets_install':
 exec { 'compass_install':
   command => 'gem install compass'
 }
+
